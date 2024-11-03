3cadc7cf31a27bb2100e60a74e173080231f61d4
Economu Teo-Antonio <teoantonioeconomu@gmail.com>
2024-11-03 17:14:55 +0200

    wip

--- server.js
+++ server.js
@@ -37,6 +37,14 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            imgSrc: ["'self'", "res.cloudinary.com"],
+        },
+    })
+);
 app.use(helmet());
 app.use(mongoSanitize());
 
