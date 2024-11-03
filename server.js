b0110805e82cfd6a7845392c5b74cf3805eacab6
Economu Teo-Antonio <teoantonioeconomu@gmail.com>
2024-11-03 15:57:16 +0200

    WIP

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
-app.use(express.static(path.resolve(__dirname, "./client/dist")));
+// app.use(express.static(path.resolve(__dirname, "./client/dist")));
+app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
