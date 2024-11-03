8e0a63a4a9c651790f0acef2ac1dd183e9695ad9
Economu Teo-Antonio <teoantonioeconomu@gmail.com>
2024-11-03 14:11:03 +0200

    WIP

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,34 +1,35 @@
 import {
-  UnauthenticatedError,
-  UnauthorizedError,
-  BadRequestError,
-} from '../errors/customErrors.js';
-import { verifyJWT } from '../utils/tokenUtils.js';
+    UnauthenticatedError,
+    UnauthorizedError,
+    BadRequestError,
+} from "../errors/customErrors.js";
+import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = (req, res, next) => {
-  const { token } = req.cookies;
-  if (!token) throw new UnauthenticatedError('authentication invalid');
+    const { token } = req.cookies;
+    if (!token) throw new UnauthenticatedError("authentication invalid");
 
-  try {
-    const { userId, role } = verifyJWT(token);
-    const testUser = userId === '663d3c8872b4be14c56f6bd8';
-    req.user = { userId, role, testUser };
-    next();
-  } catch (error) {
-    throw new UnauthenticatedError('authentication invalid');
-  }
+    try {
+        const { userId, role } = verifyJWT(token);
+        const testUser = userId === "67275e593c2e629b8f24c68f";
+        req.user = { userId, role, testUser };
+        next();
+    } catch (error) {
+        throw new UnauthenticatedError("authentication invalid");
+    }
 };
 
 export const authorizePermissions = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      throw new UnauthorizedError('Unauthorized to access this route');
-    }
-    next();
-  };
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            throw new UnauthorizedError("Unauthorized to access this route");
+        }
+        next();
+    };
 };
 
 export const checkForTestUser = (req, res, next) => {
-  if (req.user.testUser) throw new BadRequestError('Demo User. Read Only!');
-  next();
+    console.log(req.user);
+    if (req.user.testUser) throw new BadRequestError("Demo User. Read Only!");
+    next();
 };
--- server.js
+++ server.js
@@ -1,62 +1,66 @@
-import 'express-async-errors';
-import * as dotenv from 'dotenv';
+import "express-async-errors";
+import * as dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
+import express from "express";
 const app = express();
-import morgan from 'morgan';
-import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
-import cloudinary from 'cloudinary';
-import helmet from 'helmet';
-import mongoSanitize from 'express-mongo-sanitize';
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
 
 // routers
-import jobRouter from './routes/jobRouter.js';
-import authRouter from './routes/authRouter.js';
-import userRouter from './routes/userRouter.js';
+import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
 // public
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import path from 'path';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
 
 // middleware
-import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
-import { authenticateUser } from './middleware/authMiddleware.js';
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
 }
-app.use(express.static(path.resolve(__dirname, './client/dist')));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 app.use(cookieParser());
 app.use(express.json());
 app.use(helmet());
 app.use(mongoSanitize());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+    res.send("Hello World");
 });
 
-app.get('/api/v1/test', (req, res) => {
-  res.json({ msg: 'test route' });
+app.get("/api/v1/test", (req, res) => {
+    res.json({ msg: "test route" });
 });
 
-app.use('/api/v1/jobs', authenticateUser, jobRouter);
-app.use('/api/v1/users', authenticateUser, userRouter);
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
+app.use("/api/v1/auth", authRouter);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+app.get("*", (req, res) => {
+    if (process.env.NODE_ENV === "development") {
+        res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+    } else if (process.env.NODE_ENV === "production") {
+        res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+    }
 });
 
-app.use('*', (req, res) => {
-  res.status(404).json({ msg: 'not found' });
+app.use("*", (req, res) => {
+    res.status(404).json({ msg: "not found" });
 });
 
 app.use(errorHandlerMiddleware);
@@ -64,11 +68,11 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5100;
 
 try {
-  await mongoose.connect(process.env.MONGO_URL);
-  app.listen(port, () => {
-    console.log(`server running on PORT ${port}...`);
-  });
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(port, () => {
+        console.log(`server running on PORT ${port}...`);
+    });
 } catch (error) {
-  console.log(error);
-  process.exit(1);
+    console.log(error);
+    process.exit(1);
 }
